5014063b6a3876c1b2ad0db391a6420463ead2e4
Sash Sinha <sashsinha@google.com>
2024-12-01 18:27:18 -0500

    Add 2023 day 1

--- 2023/day_01.py
+++ 2023/day_01.py
@@ -57,11 +57,19 @@ def part_2(data: str) -> int:
 
 
 def main() -> None:
+  # Part 1
+  example_1_data = utils.read_data_as_lines('2023/day_01_example_1.txt')
+  print(f'Part 1 Example Answer: {part_1(example_1_data)}')
   data = utils.read_data_as_lines('2023/day_01.txt')
-  print(part_1(data))
-  print(part_2(data))
+  print(f'Part 2 Actual Answer: {part_1(data)}')
+  # Part 2
+  example_2_data = utils.read_data_as_lines('2023/day_01_example_2.txt')
+  print(f'Part 2 Example Answer: {part_2(example_2_data)}')
+  data = utils.read_data_as_lines('2023/day_01.txt')
+  print(f'Part 2 Actual Answer: {part_2(data)}')
 
 
 if __name__ == '__main__':
+  utils.create_blank_example_files(day=1, year=2023)
   utils.download_data(day=1, year=2023)
   main()
